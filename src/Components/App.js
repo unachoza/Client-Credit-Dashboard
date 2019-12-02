f12f3e9502f0254a98e5cbe8ae6f1faae94be845
arianna choza <unachoza@gmail.com>
2019-12-02 18:44:44 -0500

    scaffolding logic

--- src/Components/App.js
+++ src/Components/App.js
@@ -5,12 +5,45 @@ import { BACKEND_SERVER_PORT } from './constants';
 import './App.css';
 
 class App extends Component {
+
+	//what is state?
+	// the query from the database
+	//separate business logic from jsx
+
+
+	queryDatabase = (offset?) => {
+		if !archive
+		 return response data object 
+	 }
+	
+	archiveEntry = (entry) => {
+		change archive status from false to true
+	}
+	
+	 renderResponse = (response) => {
+		return jsx
+	 }
+	
+	loadMoreEntriesFromDatabase = (event) => {
+		increment offset
+		this.queryDatabase(offset)
+		this.renderResponse(response)
+	}
+
+
+
+
+
+
 	componentDidMount() {
 		// Example get reports request
 		fetch(`http://localhost:${BACKEND_SERVER_PORT}/api/requests?count=10&cursor=2010-11-12T02:18:22.094Z`)
 		.then(res => res.json())
 		.then(result => console.log('Example report request', result));
 	}
+
+
+
 	render() {
 		return (
 			<div className="App">
