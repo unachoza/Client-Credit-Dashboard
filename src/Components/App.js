38fe430690ae870c4e327f4efd2e7517c3546c5f
arianna choza <unachoza@gmail.com>
2019-12-03 16:57:20 -0500

    Simple Table looks real good

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
 import logo from "../logo.svg";
 import "../App.css";
-import Dashboard from "./Dashboard";
-import SimpleTable from './SimpleTable'
+import SimpleTable from "./SimpleTable";
 import { isReportArchived, archiveReport } from "../Utility/Report";
 import { getReportsFromAPI } from "../API/CreditNovaAPI";
-
 
 class App extends Component {
 	state = {
@@ -13,17 +11,17 @@ class App extends Component {
 	};
 
 	componentDidMount = async () => {
-		console.log('doing this')
+		console.log("doing this");
 		const cursor = "2000-11-12T02:18:22.094Z";
 		const count = 5;
 		let data = await getReportsFromAPI(count, cursor);
 		this.setState({ data });
-		isReportArchived(this.state.data)
-		let archivedReport = await archiveReport("f0549f22-ce07-11e7-8caa-db5e161d6850")
-		this.setState({ thisOne: archivedReport })
-		
+		isReportArchived(this.state.data);
+		let archivedReport = await archiveReport(
+			"f0549f22-ce07-11e7-8caa-db5e161d6850"
+		);
+		this.setState({ thisOne: archivedReport });
 	};
-
 
 	render() {
 		const { data, thisOne } = this.state;
@@ -33,17 +31,8 @@ class App extends Component {
 					<img src={logo} className="App-logo" alt="logo" />
 					<h1 className="App-title">Welcome to Nova's code challenge!</h1>
 				</header>
-				{thisOne ? (
-					<p>
-						This report has been archvied: {thisOne[0].firstName} {thisOne[0].lastName}
-					</p>
-				) : (
-					<p>loading</p>
-				)}
-		{data.length>0 && <SimpleTable reports={data} />}
-				
-				{data && data.map(data => <Dashboard key={data.id} reports={data} />)}
-				
+
+				{data.length > 0 ? <SimpleTable reports={data} /> : <p>Loading ... </p>}
 			</div>
 		);
 	}
